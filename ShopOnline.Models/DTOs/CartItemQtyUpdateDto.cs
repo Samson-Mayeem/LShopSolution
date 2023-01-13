29c5f77f420d2688071960fb1bf003ec4961f303
Samson-Mayeem <63527034+Samson-Mayeem@users.noreply.github.com>
2023-01-13 12:29:27 +0000

    .*

--- ShopOnline.Models/DTOs/CartItemQtyUpdateDto.cs
+++ ShopOnline.Models/DTOs/CartItemQtyUpdateDto.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ShopOnline.Models.DTOs
+namespace Mark.Up.Hub.Models.DTOs
 {
     public class CartItemQtyUpdateDto
     {
